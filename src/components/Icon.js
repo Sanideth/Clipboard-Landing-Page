be39bb01bd95d58d404b13564dfb20ec56a29179
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-05 03:00:43 +0400

    added screen reader only span for the blank anchors

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,9 @@ import sprite from "../img/icomoon/symbol-defs.svg";
 
 const Icon = (props) => {
   return (
-    <a href="https://github.com/Sanideth" name="Social Media Icon Anchor">
+    <a href="https://github.com/Sanideth">
+      {" "}
+      <span class="sr-only">Social Media Icon</span>
       <svg className={`social-media social-media--${props.className}`}>
         <use href={`${sprite}#icon-${props.socialMedia}`}></use>
       </svg>
