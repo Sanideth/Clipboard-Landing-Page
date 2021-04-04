6cc1d6cfcb84a518e5e9de70fbbe8773ef429035
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-05 02:54:53 +0400

    added name attribute to blank anchors

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,7 @@ import sprite from "../img/icomoon/symbol-defs.svg";
 
 const Icon = (props) => {
   return (
-    <a href="https://github.com/Sanideth">
+    <a href="https://github.com/Sanideth" name="Social Media Icon Anchor">
       <svg className={`social-media social-media--${props.className}`}>
         <use href={`${sprite}#icon-${props.socialMedia}`}></use>
       </svg>
