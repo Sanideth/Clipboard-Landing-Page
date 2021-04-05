137cac63f8f306f7f0e1a7e4e5eaa2fd3fde95a5
Sandro Bochorishvili <Sanideth@gmail.com>
2021-04-05 23:30:21 +0400

    fixed .sr-only class for icons, added styles to hide them

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,8 +2,7 @@ import sprite from "../img/icomoon/symbol-defs.svg";
 
 const Icon = (props) => {
   return (
-    <a href="https://github.com/Sanideth">
-      {" "}
+    <a href="https://github.com/Sanideth" className="icon-link">
       <span class="sr-only">Social Media Icon</span>
       <svg className={`social-media social-media--${props.className}`}>
         <use href={`${sprite}#icon-${props.socialMedia}`}></use>
